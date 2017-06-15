75a580955a4b78c327f581ab0925b98053c3f8ec
Andy <Andy@gmail.com>
2017-06-15 14:01:06 +0800

    酷欧天气.

---
+++ app/src/main/java/com/coolweather/android/util/HttpUtil.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import static android.R.string.ok;
+
+/**
+ * Created by Administrator on 2017/6/8.
+ */
+
+public class HttpUtil {
+    public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
